57730f07598389b678cccaf0fe6e5f11d7d7b266
Simon <simon.michael.te15d@gmail.com>
2022-11-30 15:37:52 +0100

    added dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Links from '../Links/Links';
 import './Dropdown.scss';
+import CrossIcon from '../../assets/icon-cross.svg';
 
 const Dropdown = ({ dropdown, toggleDropdown }) => {
     return (
         <div className={`dropdown ${dropdown ? 'active-dropdown' : ''}`}>
             <div className='dropdown-box'>
+                <button
+                    onClick={() => {
+                        toggleDropdown(false);
+                    }}
+                    className='dropdown-close-btn'
+                >
+                    <img src={CrossIcon} />
+                </button>
                 <Links />
             </div>
         </div>
