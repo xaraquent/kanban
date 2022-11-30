2aa203c3c64661fa5a4eea0c5381dd782a83818e
Simon <simon.michael.te15d@gmail.com>
2022-11-30 11:27:31 +0100

    some style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Board from './components/Board/Board';
 import Sidebar from './components/Sidebar/Sidebar';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
     return (
         <div className='App'>
+            <Navbar />
             <main>
                 <Sidebar />
                 <Board />
